4ae86420b6b68c386092a2db5ba57a614ef5b10c
Yam Jam <52462377+Hi5DaWorld@users.noreply.github.com>
2019-11-09 11:32:54 -0500

    Update Main&tool.java
    ye

--- Main&tool.java
+++ Main&tool.java
@@ -1,16 +1,26 @@
-/******************************************************************************
-
-yAAAAÀ
-
-*******************************************************************************/
 public class Main
 {
 	public static void main(String[] args) {
+	    
+	    tool myTools[] = new tool[2];
+	    for (int i=0;i<myTools.length;i++){
+	        System.out.println("i demand u to enter a food:");
+	        String input = System.console().readLine();
+	        myTools[i] = new tool(input,i);
+	    }
+	    
+	    for (int i=0;i<myTools.length;i++){
+	        System.out.println(myTools[i].getType()+" location: "+myTools[i].getLocation());
+	    }
+	    
+	    System.out.println();
 	    
 	    tool cake = new tool("cake"+"=",1);
 	    tool cookie = new tool("cookie"+"=",3);
 		System.out.println(cake.getType() + cake.getLocation());
 		System.out.println(cookie.getType() + cookie.getLocation());
+		System.out.println();
+		
 		System.out.println("u eat a cookie");
 		cookie.setLocation(cookie.getLocation()-1);
 		System.out.println(cookie.getType() + cookie.getLocation());
@@ -20,8 +30,7 @@ public class Main
 	}
 }
 
-//WARNING
-//tool starts here
+//TOOLS START HERE!!
 
 public class tool{
     private String type;
